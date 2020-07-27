89ee6f9ba70469a63ea5e345eb87741ce4f9657a
Abhishek Kumar <avi.kr16@gmail.com>
2020-07-27 18:27:40 +0530

    initial commit

---
+++ app/src/main/java/com/abhishek/paidcourseapp/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.abhishek.paidcourseapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abhishek.paidcourseapp.R.layout
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
