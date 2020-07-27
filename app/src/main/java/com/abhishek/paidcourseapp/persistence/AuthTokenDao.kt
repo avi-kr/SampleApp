d4418d9a475e4b21ccb5ff9c61302d2fafd24b0d
Abhishek Kumar <avi.kr16@gmail.com>
2020-07-27 23:47:51 +0530

    room persistence setup

--- app/src/main/java/com/abhishek/paidcourseapp/persistence/AuthTokenDao.kt
+++ app/src/main/java/com/abhishek/paidcourseapp/persistence/AuthTokenDao.kt
@@ -1,10 +1,23 @@
 package com.abhishek.paidcourseapp.persistence
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.abhishek.paidcourseapp.models.AuthToken
+
 /**
  * Created by Abhishek Kumar on 27/07/20.
  * (c)2020 VMock. All rights reserved.
  */
 
+@Dao
 interface AuthTokenDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(authToken: AuthToken) : Long
+
+    @Query("UPDATE auth_token SET token = null WHERE account_pk = :pk")
+    fun nullifyToken(pk: Int) : Int
 
 }
