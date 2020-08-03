ad5386e26d135168999d05b75c9bea96206ec6ab
Abhishek Kumar <avi.kr16@gmail.com>
2020-08-03 17:56:51 +0530

    Password Reset

--- app/src/main/java/com/abhishek/sampleapp/ui/auth/ForgotPasswordFragment.kt
+++ app/src/main/java/com/abhishek/sampleapp/ui/auth/ForgotPasswordFragment.kt
@@ -1,12 +1,64 @@
 package com.abhishek.sampleapp.ui.auth
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.TranslateAnimation
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.findNavController
 import com.abhishek.sampleapp.R
+import com.abhishek.sampleapp.ui.DataState
+import com.abhishek.sampleapp.ui.DataStateChangeListener
+import com.abhishek.sampleapp.ui.Response
+import com.abhishek.sampleapp.ui.ResponseType.Dialog
+import com.abhishek.sampleapp.ui.auth.ForgotPasswordFragment.WebAppInterface.OnWebInteractionCallback
+import com.abhishek.sampleapp.util.Constants
+import kotlinx.android.synthetic.main.fragment_forgot_password.parent_view
+import kotlinx.android.synthetic.main.fragment_forgot_password.password_reset_done_container
+import kotlinx.android.synthetic.main.fragment_forgot_password.return_to_launcher_fragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ForgotPasswordFragment : BaseAuthFragment() {
+
+    lateinit var webView: WebView
+
+    lateinit var stateChangeListener: DataStateChangeListener
+
+    val webInteractionCallback = object : OnWebInteractionCallback {
+
+        override fun onError(errorMessage: String) {
+            Log.e(TAG, "onError: $errorMessage")
+
+            val dataState = DataState.error<Any>(
+                response = Response(errorMessage, Dialog())
+            )
+            stateChangeListener.onDataStateChange(
+                dataState = dataState
+            )
+        }
+
+        override fun onSuccess(email: String) {
+            Log.d(TAG, "onSuccess: a reset link will be sent to $email.")
+            onPasswordResetLinkSent()
+        }
+
+        override fun onLoading(isLoading: Boolean) {
+            Log.d(TAG, "onLoading... ")
+            CoroutineScope(Dispatchers.Main).launch {
+                stateChangeListener.onDataStateChange(
+                    DataState.loading(isLoading = isLoading, cacheData = null)
+                )
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,4 +68,90 @@ class ForgotPasswordFragment : BaseAuthFragment() {
         return inflater.inflate(R.layout.fragment_forgot_password, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        webView = view.findViewById(R.id.webview)
+
+        loadPasswordResetWebView()
+
+        return_to_launcher_fragment.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    fun loadPasswordResetWebView() {
+        stateChangeListener.onDataStateChange(
+            DataState.loading(isLoading = true, cacheData = null)
+        )
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                stateChangeListener.onDataStateChange(
+                    DataState.loading(isLoading = false, cacheData = null)
+                )
+            }
+        }
+        webView.loadUrl(Constants.PASSWORD_RESET_URL)
+        webView.settings.javaScriptEnabled = true
+        webView.addJavascriptInterface(WebAppInterface(webInteractionCallback), "AndroidTextListener")
+    }
+
+    class WebAppInterface
+    constructor(
+        private val callback: OnWebInteractionCallback
+    ) {
+
+        private val TAG: String = "AppDebug"
+
+        @JavascriptInterface
+        fun onSuccess(email: String) {
+            callback.onSuccess(email)
+        }
+
+        @JavascriptInterface
+        fun onError(errorMessage: String) {
+            callback.onError(errorMessage)
+        }
+
+        @JavascriptInterface
+        fun onLoading(isLoading: Boolean) {
+            callback.onLoading(isLoading)
+        }
+
+        interface OnWebInteractionCallback {
+
+            fun onSuccess(email: String)
+
+            fun onError(errorMessage: String)
+
+            fun onLoading(isLoading: Boolean)
+        }
+    }
+
+    fun onPasswordResetLinkSent() {
+        CoroutineScope(Dispatchers.Main).launch {
+            parent_view.removeView(webView)
+            webView.destroy()
+
+            val animation = TranslateAnimation(
+                password_reset_done_container.width.toFloat(),
+                0f,
+                0f,
+                0f
+            )
+            animation.duration = 500
+            password_reset_done_container.startAnimation(animation)
+            password_reset_done_container.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            stateChangeListener = context as DataStateChangeListener
+        } catch (e: ClassCastException) {
+            Log.e(TAG, "$context must implement DataStateChangeListener")
+        }
+    }
 }
--- app/src/main/java/com/abhishek/sampleapp/util/Constants.kt
+++ app/src/main/java/com/abhishek/sampleapp/util/Constants.kt
@@ -7,9 +7,10 @@ package com.abhishek.sampleapp.util
 
 class Constants {
 
-    companion object{
+    companion object {
 
         const val BASE_URL = "https://open-api.xyz/api/"
+        const val PASSWORD_RESET_URL: String = "https://open-api.xyz/password_reset/"
 
         const val NETWORK_TIMEOUT = 3000L
         const val TESTING_NETWORK_DELAY = 0L // fake network delay for testing
